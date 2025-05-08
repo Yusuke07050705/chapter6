eb1e1a2144b9c7f3c837bbc0a5022f772030d3fb
Yusuke Satomi <yusuke07.0507@gmail.com>
2025-05-08 19:06:10 +0900

    課題1実装

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TaskOne from './taskOne';
+import { posts } from './data/posts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TaskOne data={posts} />
   </React.StrictMode>
 );
 
