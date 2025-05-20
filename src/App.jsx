ece258dac30a6ec66b5eeb7ce2752beff91f7125
Yusuke Satomi <yusuke07.0507@gmail.com>
2025-05-20 17:58:08 +0900

    課題4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Home } from "./components/Home";
 import { Header } from "./components/Home/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PostDetail } from "./components/Home/PostDetail";
+import Contact from "./components/Home/Contact/Contact";
 
 export default function App({ data }) {
   return (
@@ -10,6 +11,7 @@ export default function App({ data }) {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<PostDetail />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home/Header/index.jsx
+++ src/components/Home/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <p>Blog</p>
-      <p>contact</p>
+      <Link to = "/" className={styles.link}><p>Blog</p></Link>
+      <Link to = "/contact" className={styles.link}><p>お問い合わせ</p></Link>
     </header>
   );
 };
